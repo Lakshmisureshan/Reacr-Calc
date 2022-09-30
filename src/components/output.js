b9595e7ac11a48c6aafe70310fc8e123591531e0
Lakshmisureshan <113295897+Lakshmisureshan@users.noreply.github.com>
2022-09-30 09:35:29 +0400

    1 file modified

--- src/components/output.js
+++ src/components/output.js
@@ -3,8 +3,8 @@ import OutputRow from './outputRow';
 const Output = (props) => {
   return (
     <div>
-      <OutputRow value={99} text-size={{ fontSize: '20px' }} />
-      <OutputRow value={100} text-size={{ fontSize: '20px' }} />
+      <OutputRow value={props.answer} text-size={{ fontSize: '20px' }} />
+      <OutputRow value={props.user} text-size={{ fontSize: '20px' }} />
     </div>
   );
 };
